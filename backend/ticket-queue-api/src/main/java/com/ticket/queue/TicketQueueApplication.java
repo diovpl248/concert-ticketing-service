4ff118e9ae564f1d8790170c27f9df0d9f0ef901
diovpl248 <lpvoid9034@gmail.com>
2026-03-05 20:00:37 +0900

    Fix:queue-api db 의존성 오류 수정
    - db 사용 누락된 부분 추가

--- backend/ticket-queue-api/src/main/java/com/ticket/queue/TicketQueueApplication.java
+++ backend/ticket-queue-api/src/main/java/com/ticket/queue/TicketQueueApplication.java
@@ -2,15 +2,14 @@ package com.ticket.queue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableScheduling
-@SpringBootApplication(
-	exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class},
-	scanBasePackages = {"com.ticket.core.common", "com.ticket.core.domain.queue", "com.ticket.core.config", "com.ticket.queue"}
-)
+@SpringBootApplication(scanBasePackages = {"com.ticket.core", "com.ticket.queue"})
+@EntityScan(basePackages = "com.ticket.core")
+@EnableJpaRepositories(basePackages = "com.ticket.core")
 public class TicketQueueApplication {
 
 	public static void main(String[] args) {
